ff023927734056a8e069be1d2c9634835d720ea1
fthabet1 <thabetfuad@gmail.com>
2021-07-26 01:28:47 -0400

    Created practice python files

---
+++ python/lists.py
@@ -0,0 +1,9 @@
+# Define a list of names
+names = ["Luffy", "Zoro", "Sanji", "Nami", "Ussop", "Chopper", "Robin", "Franky"]
+
+print(names[0])
+
+names.append("Brook")
+names.sort()
+
+print(names)
---
+++ python/sequences.py
@@ -0,0 +1,12 @@
+name = "Wahib"
+
+print(name[0])
+
+names = ["Fuad", "Zayd", "Yacine"]
+
+print(names[0])
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
